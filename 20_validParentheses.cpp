5231a7b47f9463f5c9798578d0d73ac6b7dcb16b
JonsenMa <jonsen.ma@gmail.com>
2020-02-22 00:14:58 +0800

    20_validParentheses
    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    
    An input string is valid if:
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Note that an empty string is also considered valid.
    
    Example 1:
    Input: "()"
    Output: true
    
    Example 2:
    Input: "()[]{}"
    Output: true
    
    Example 3:
    Input: "(]"
    Output: false
    
    Example 4:
    Input: "([)]"
    Output: false
    
    Example 5:
    Input: "{[]}"
    Output: true

---
+++ 20_validParentheses.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+#include<map>
+#include<string>
+#include<stack>
+
+using namespace std;
+
+bool isVaild(string s) {
+	map<string, string> map;
+	map.insert(pair<string, string>("(", ")"));
+	map.insert(pair<string, string>("[", "]"));
+	map.insert(pair<string, string>("{", "}"));
+	stack<string> stack;
+
+	int size = s.size();
+	if (size == 0)
+		return true;
+	for (int i = 0; i < size; i++){
+		string temp = s.substr(i, 1);
+		if (map.find(temp) != map.end()){
+			stack.push(temp);
+		}
+		else {
+			string top = map[stack.top()];
+			if (top == temp) {
+				stack.pop();
+				continue;
+			}
+			else
+				return false;
+		}
+	}
+	if (stack.empty())
+		return true;
+	else
+		return false;
+}
+
+int main() {
+	string s;
+	cin >> s;
+	if (isVaild(s))
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+}
