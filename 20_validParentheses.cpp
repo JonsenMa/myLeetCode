da991d4695860b9fce82a7baa28b391fdd40aac3
JonsenMa <mjx690621749@163.com>
2020-02-22 00:33:15 +0800

    Update 20_validParentheses.cpp
    加入题目介绍和注释

--- 20_validParentheses.cpp
+++ 20_validParentheses.cpp
@@ -1,4 +1,31 @@
-﻿#include<iostream>
+/*Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+Input: "()"
+Output: true
+
+Example 2:
+Input: "()[]{}"
+Output: true
+
+Example 3:
+Input: "(]"
+Output: false
+
+Example 4:
+Input: "([)]"
+Output: false
+
+Example 5:
+Input: "{[]}"
+Output: true*/
+
+#include<iostream>
 #include<map>
 #include<string>
 #include<stack>
@@ -6,16 +33,20 @@
 using namespace std;
 
 bool isVaild(string s) {
+	//用map存放所有可能出现的括号组合，左括号为key，右括号为value
 	map<string, string> map;
 	map.insert(pair<string, string>("(", ")"));
 	map.insert(pair<string, string>("[", "]"));
 	map.insert(pair<string, string>("{", "}"));
-	stack<string> stack;
+	stack<string> stack;//使用stack依次存入输入的括号字符串
 
 	int size = s.size();
 	if (size == 0)
-		return true;
+		return true;//边界条件，当没有输入字符串时，认为合法
 	for (int i = 0; i < size; i++){
+		/*依次判断每个字符，若是map中有的左括号，则将该符号压入栈中，
+		若不是map中的左括号，则将栈顶符号与之比较，若为一对，则弹出栈顶符号，直到栈空说明合法；
+		若不为一对，则不合法*/
 		string temp = s.substr(i, 1);
 		if (map.find(temp) != map.end()){
 			stack.push(temp);
