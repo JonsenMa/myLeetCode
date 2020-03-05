04160a5ebb43a6818c166c6547cec6da9dc6d7a1
JonsenMa <jonsen.ma@gmail.com>
2020-03-05 23:49:12 +0800

    Add files via upload
    简单买买股票问题

---
+++ 121_best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		if (prices.empty()) return 0;
+		auto min = prices[0];
+		auto profit = 0;
+		for (auto i = 1; i < prices.size(); ++i) {
+			if (prices[i] > prices[i - 1]) {
+				profit = max(profit, prices[i] - min);
+			}
+			else {
+				min = std::min(min, prices[i]);;
+			}
+		}
+		return profit;
+	}
+};
+
+int main()
+{
+	Solution s;
+	vector<int> prices{7,1,5,3,6,4};
+	int max = s.maxProfit(prices);
+	cout << "max profit: " << max << endl;
+}
